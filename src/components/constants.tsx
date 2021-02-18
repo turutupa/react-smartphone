a28b2362c3ca96671eb9c5233003690ed086f43c
Turutupa <adc.batros@gmail.com>
2021-02-18 13:32:59 +0100

    changed default value for phone size

--- src/components/constants.tsx
+++ src/components/constants.tsx
@@ -1,5 +1,5 @@
-export const phoneWidth = 480;
-export const phoneHeight = 800;
+export const phoneWidth = 380;
+export const phoneHeight = 700;
 export const phoneColor = '#f3f0dd';
 export const phoneSecondaryColor = '#b0b3b2';
 export const dark = '#374D5C';
